de6a9ef968678ece135e70626a633f11abd41a5b
sjinwoo <sjw5953@naver.com>
2023-03-28 15:19:18 +0900

    Prepare data JSON files

---
+++ data_configuration.py
@@ -0,0 +1,13 @@
+import json
+
+ko = json.load(open('.\\ko_alpaca_data.json', 'rt', encoding='UTF8'))
+en = json.load(open('.\\en_alpaca_data.json', 'rt', encoding='UTF8'))
+total = ko + en
+
+# with open('alpaca_data.json', 'w', encoding='UTF8') as file:
+#     json.dump(total, file, indent="\t", ensure_ascii=False)
+
+with open('.\\alpaca_data.json', 'r', encoding='UTF8') as file:
+    data = json.load(file)
+
+print(len(data))
