786501d6cb2791d7ae5b6afacdbfc958b6d8aeee
sjinwoo <sjw5953@naver.com>
2023-05-30 21:42:55 +0900

    jsonl to json

--- dataset/data_converter.py
+++ dataset/data_converter.py
@@ -4,17 +4,27 @@ from  collections import OrderedDict
 
 text_form = "### 질문: {instruction} ### 답변: {output}"
 
+
+# with jsonlines.open('.\\KoAlpaca_v1.1.jsonl', 'r') as jsonl:
+#     json_list = list(jsonl)
+
+# for json_str in json_list:
+#     result = json.loads(json_str)
+#     print(f"result: {result}")
+#     print(isinstance(result, dict))
+
+# with open(".\\odego_data.json", 'w') as file:
+#     json.dump(result, file, indent='\t', ensure_ascii=False)
+
 dataset = list()
-with jsonlines.open(".\\KoAlpaca_v1.1.jsonl") as data:
+with jsonlines.open(".\\KoAlpaca_v1.1.jsonl", 'r') as data:
     for line in data.iter():
         data_dict = OrderedDict()
         data = text_form.format(instruction=line['instruction'], output=line['output'])
         data_dict["text"] = data
         dataset.append(data_dict)
             
-print(dataset[:10])
-
-with jsonlines.open(".\\odego_data.json", 'w') as file:
+with open(".\\odego_data.json", 'w') as file:
     json.dump(data_dict, file, indent='\t', ensure_ascii=False)
 
     
