c359fe9bc7fdae4380472afdb7c1973cc098ecd5
sjinwoo <sjw5953@naver.com>
2023-05-31 12:05:47 +0900

    Update dataset by KoAlpaca_v1.1

--- dataset/data_converter.py
+++ dataset/data_converter.py
@@ -4,28 +4,28 @@ from  collections import OrderedDict
 
 text_form = "### 질문: {instruction} ### 답변: {output}"
 
+# dataset = list()
+# with jsonlines.open(".\\KoAlpaca_v1.1.jsonl", 'r') as data:
+#     for line in data.iter():
+#         data_dict = OrderedDict()
+#         data = text_form.format(instruction=line['instruction'], output=line['output'])
+#         data_dict["text"] = data
+#         dataset.append(data_dict)
+#     print(dataset)
+            
+# with open(".\\odego_data.json", 'w', encoding="utf-8") as file:
+#     json.dump(dataset, file, indent='\t', ensure_ascii=False)
 
-# with jsonlines.open('.\\KoAlpaca_v1.1.jsonl', 'r') as jsonl:
-#     json_list = list(jsonl)
-
-# for json_str in json_list:
-#     result = json.loads(json_str)
-#     print(f"result: {result}")
-#     print(isinstance(result, dict))
-
-# with open(".\\odego_data.json", 'w') as file:
-#     json.dump(result, file, indent='\t', ensure_ascii=False)
 
 dataset = list()
 with jsonlines.open(".\\KoAlpaca_v1.1.jsonl", 'r') as data:
     for line in data.iter():
         data_dict = OrderedDict()
-        data = text_form.format(instruction=line['instruction'], output=line['output'])
-        data_dict["text"] = data
+        data_dict["instruction"] = line['instruction']
+        data_dict["input"] = ''
+        data_dict["output"] = line['output']
         dataset.append(data_dict)
-            
-with open(".\\odego_data.json", 'w') as file:
-    json.dump(data_dict, file, indent='\t', ensure_ascii=False)
 
-    
+with open(".\\odego_data.json", 'w', encoding="utf-8") as file:
+    json.dump(dataset, file, indent='\t', ensure_ascii=False)
 
